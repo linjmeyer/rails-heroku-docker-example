2a26eb997292694d8890ec3a4033972cd4c0c566
Lin Meyer <lin.meyer@trustedhealth.com>
2022-07-25 12:48:23 -0500

    Added sidekiq stuff

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem "jbuilder"
 
 # Sidekiq
 gem 'sidekiq', '~>6.0.0'
+gem "sidekiq-cron"
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+    def index
+        logger.info("Running Lin's manual worker")
+        Sidekiq::Client.push('class' => 'ManualWorker', 'args' => [])
+    end
 end
